9acaa1a353b3e9c2b9b9bc853ad96fac7a67e626
John López <johnalbh@gmail.com>
2022-10-03 12:06:51 +0200

    Creation NewTask components

---
+++ CodeAcademy/Hooks/hooks/src/Components/NewTask.js
@@ -0,0 +1,14 @@
+import React from 'react';
+export default function NewTask({ newTask, handleChange, handleSubmit }) {
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name='title' placeholder='New Task' value={newTask.title || ''} onChange={handleChange} />
+      {!newTask.title ? null : (
+        <>
+          <textarea name='description' placeholder='Details...' value={newTask.description || ''} onChange={handleChange} />
+          <button type='submit'> Add Task</button>
+        </>
+      )}
+    </form>
+  );
+}
