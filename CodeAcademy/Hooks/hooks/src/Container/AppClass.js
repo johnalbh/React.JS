6ad448a5362346246d873c2fe79276da65a1df10
John López <johnalbh@gmail.com>
2022-10-03 12:32:13 +0200

    Implementation Handle Methods

--- CodeAcademy/Hooks/hooks/src/Container/AppClass.js
+++ CodeAcademy/Hooks/hooks/src/Container/AppClass.js
@@ -8,13 +8,43 @@ export default class AppClass extends Component {
       newTask: {},
       allTasks: [{ title: 'John', description: 'Lopez', id: 1 }]
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState((prevState) => ({
+      ...prevState,
+      newTask: {
+        ...prevState.newTask,
+        [name]: value,
+        id: Date.now()
+      }
+    }));
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    if (!this.state.newTask.title) return;
+    this.setState((prevState) => ({
+      allTasks: [prevState.newTask, ...prevState.allTasks],
+      newTask: {}
+    }));
+  }
+  handleDelete(taskIdRemove) {
+    this.setState((prevState) => ({
+      ...prevState,
+      allTasks: prevState.allTasks.filter((task) => task.id !== taskIdRemove)
+    }));
+  }
+
   render() {
     return (
       <main>
         <h3>Task</h3>
-        <NewTask newTask={this.state.newTask} handleChange={() => {}} handleSubmit={() => {}} />
-        <TaskList allTasks={this.state.allTasks} handleDelete={() => {}} />
+        <NewTask newTask={this.state.newTask} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
+        <TaskList allTasks={this.state.allTasks} handleDelete={this.handleDelete} />
       </main>
     );
   }
