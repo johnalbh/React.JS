6edb24adf8d21499c559760fbbe2b84c738eee02
John López <johnalbh@gmail.com>
2022-10-03 12:18:12 +0200

    Implementarion TaskList Components

---
+++ CodeAcademy/Hooks/hooks/src/Components/TaskList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+export default function TaskList({ allTasks, handleDelete }) {
+  return (
+    <ul>
+      {allTasks.map(({ title, description, id }) => (
+        <li key={id}>
+          <div>
+            <h2>{title}</h2>
+            <button onClick={() => handleDelete(id)}>X</button>
+          </div>
+          {!description ? null : <p>{description}</p>}
+        </li>
+      ))}
+    </ul>
+  );
+}
--- CodeAcademy/Hooks/hooks/src/Container/AppClass.js
+++ CodeAcademy/Hooks/hooks/src/Container/AppClass.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import NewTask from '../Components/NewTask';
+import TaskList from '../Components/TaskList';
 export default class AppClass extends Component {
   constructor(props) {
     super(props);
     this.state = {
       newTask: {},
-      allTasks: []
+      allTasks: [{ title: 'John', description: 'Lopez', id: 1 }]
     };
   }
   render() {
     return (
       <main>
         <h3>Task</h3>
+        <NewTask newTask={this.state.newTask} handleChange={() => {}} handleSubmit={() => {}} />
+        <TaskList allTasks={this.state.allTasks} handleDelete={() => {}} />
       </main>
     );
   }
