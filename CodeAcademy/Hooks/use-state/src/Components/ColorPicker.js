79851e25fb0e5f46a9edfe1a6f43c4c2c25639cd
John López <johnalbh@gmail.com>
2022-10-04 10:31:01 +0200

    Ajuste componente ColorPicker

--- CodeAcademy/Hooks/use-state/src/Components/ColorPicker.js
+++ CodeAcademy/Hooks/use-state/src/Components/ColorPicker.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 export default function ColorPicker() {
   const [color, setColor] = useState();
-  const divStyle = { backgroundColor: color };
+  const divStyle = { backgroundColor: color, color: 'white' };
 
   return (
     <div style={divStyle} class='prueba'>
-      <p> The color is {color}</p>
-      <button onClick={() => setColor('aquamarine')}>Aquamarine</button>
+      <h1> The color is {color}</h1>
+      <button onClick={() => setColor('Blue')}>Blue</button>
+      <button onClick={() => setColor('BlueViolet')}>BlueViolet</button>
+      <button onClick={() => setColor('Chartreuse')}>Chartreuse</button>
+      <button onClick={() => setColor('CornflowerBlue')}>CornflowerBlue</button>
+      <button onClick={() => setColor('Red')}>Red</button>
     </div>
   );
 }
